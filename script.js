c3b05bb0f7313f6ee50772959287e027f4061177
deogratiasimanuel <119482000+deogratiasimanuel@users.noreply.github.com>
2024-11-13 00:56:54 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function sendMail(){
+    var params = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        phone : document.getElementById("phone").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }; 
+
+
+const serviceID = "service_vkh7tfg";
+const templateID = "template_gitk1op";
+
+emailjs
+   .send(serviceID, templateID, params)
+   .then((res) => {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("subject").value = "";
+    document.getElementById("message").value = "";
+    console.log(res);
+    alert("Pesan Terkirim");
+   })
+   .catch((err) => console.log(err));
+}
